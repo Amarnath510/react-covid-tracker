3f3d6f97e6bb93dc32a1c1c80031bc34c10f350d
camarnath <camarnath@castlighthealth.com>
2020-07-02 22:34:29 +0530

    footer font

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,13 +5,13 @@ export class Footer extends Component {
   render() {
     return (
       <footer className="covid-tracker-footer">
-        <p className="text-white">Data Source &nbsp;
+        <p className="text-light-gray">Data Source &nbsp;
           <a href="https://documenter.getpostman.com/view/10724784/SzYXXKmA?version=latest" target="_blank" rel="noopener noreferrer" className="external-link link">Postman COVID19-India API</a>
         </p>
-        <p className="text-white">Developed using &nbsp;
+        <p className="text-light-gray">Developed using &nbsp;
           <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" className="external-link link">React</a>
         </p>
-        <p className="text-light">
+        <p className="text-light-gray">
           2020 &copy; Amarnath Chandana
         </p>
       </footer>
